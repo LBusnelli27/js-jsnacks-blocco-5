628fddb253fbb998ba063204b26d0c79f1760874
Busna27 <lucabusnelli2001@gmail.com>
2022-03-11 14:50:10 +0100

    jsnack1 - done

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,41 +1,72 @@
+// ***** FUNCTION *****
+
+/**
+ * Function that gen random string letter (only capitalize letters)
+ * 
+ * @param {*} length Lenght of the string that you want to generate
+ * @returns Return the string
+ */
+function makeid(length) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let charactersLength = characters.length;
+  
+  for ( let i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+    charactersLength));
+  }
+  
+  return result;
+}
+
+/**
+ * Function that gen random string numbers
+ * 
+ * @param {*} length Lenght of the string that you want to generate
+ * @returns Return the string
+ */
+function makeNum(length) {
+  let result = '';
+  let characters = '0123456789';
+  let charactersLength = characters.length;
+  
+  for ( let i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+    charactersLength));
+  }
+  
+  return result;
+}
+
+/**
+ * 
+ * @returns Return the player code
+ */
+function genRandomPlayerCode() {
+  return makeid(3) + makeNum(3);
+}
+
+/**
+ * 
+ * @returns Random int from 0 to 99
+ */
+function genRandomInt() {
+  return Math.floor(Math.random() * 99);
+}
+
+// ***** END FUNCTION *****
+
+
 let player = {
     playerCode : '',
     name : 'Luca',
-    lastName : 'Perini',
-    eta : '32',
+    lastName : 'Busna',
+    eta : '21',
     averagePointsGame : '',
     percSuccessShots : '',
 }
+player.playerCode = genRandomPlayerCode();
+player.averagePointsGame = genRandomInt();
+player.percSuccessShots = genRandomInt();
 
-
-function makeid(length) {
-    let result = '';
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    let charactersLength = characters.length;
-
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
-    charactersLength));
-    }
-
-    return result;
-}
-
-function makeNum(length) {
-    let result = '';
-    let characters = '0123456789';
-    let charactersLength = characters.length;
-
-    for ( let i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
-    charactersLength));
-    }
-
-    return result;
-}
-
-function genPlayerCode() {
-    return makeid(3) + makeNum(3);
-}
-
-console.log(player)
+console.table(player)
