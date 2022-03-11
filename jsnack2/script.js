96c22df971903736a7eee75facca2727e1d67e0f
Busna27 <lucabusnelli2001@gmail.com>
2022-03-11 14:55:08 +0100

    jsnack2 - fixed

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -28,6 +28,36 @@ const carsArray = [
     name : 'Fiat',
     model : 'Panda 4X4',
     typePower : 'Metano'
+  },
+  {
+    name : 'BMW',
+    model : 'M4',
+    typePower : 'Benzina'
+  },
+  {
+    name : 'Opel',
+    model : 'Astra',
+    typePower : 'Diesel'
+  },
+  {
+    name : 'Fiat',
+    model : 'Punto',
+    typePower : 'Gpl'
+  },
+  {
+    name : 'Skoda',
+    model : 'Fabia',
+    typePower : 'Metano'
+  },
+  {
+    name : 'Lamborghini',
+    model : 'Aventador',
+    typePower : 'Benzina'
+  },
+  {
+    name : 'Bugatti',
+    model : 'Chiron',
+    typePower : 'Benzina'
   }
 ]
 
@@ -49,6 +79,7 @@ const arrayAltri = carsArray.filter((element) => {
   }
 });
 
-console.log(arrayBenzina);
-console.log(arrayDiesel);
-console.log(arrayAltri);
+console.log(carsArray);
+console.table(arrayBenzina);
+console.table(arrayDiesel);
+console.table(arrayAltri);
