88ff7d670c91697f41715c296db928615bbfd3d6
Busna27 <lucabusnelli2001@gmail.com>
2022-03-14 10:04:34 +0100

    refactor age in js

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -2,22 +2,22 @@ const arrayUser = [
   {
     name : 'Luca',
     lastName : 'Busna',
-    eta : '21'
+    age : '21'
   },
   {
     name : 'Giulia',
     lastName : 'Bonny',
-    eta : '21'
+    age : '21'
   },
   {
     name : 'Andrea',
     lastName : 'Rossi',
-    eta : '4'
+    age : '4'
   },
   {
     name : 'Federico',
     lastName : 'Rossi',
-    eta : '10'
+    age : '10'
   }
 ];
 
@@ -27,7 +27,7 @@ const arrayCanDrive = [
 
 
 arrayUser.filter((element) => {
-  if(element.eta > 18) {
+  if(element.age > 18) {
     arrayCanDrive.push({
       phrase : element.name + ' ' + element.lastName + ' ' + 'can drive'
     })
